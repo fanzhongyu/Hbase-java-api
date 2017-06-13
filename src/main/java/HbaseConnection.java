66d19c41432cb2721a72d1d4283ab9fefb2c385e
fanzhongyu <fanzhongyu@tongfangcloud.com>
2017-06-13 11:35:13 +0800

    1.新增Put功能和Get功能 2.替换掉被废除的方法，使用1.2新版本API

--- src/main/java/HbaseConnection.java
+++ src/main/java/HbaseConnection.java
@@ -1,10 +1,9 @@
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.io.compress.Compression;
 import org.apache.hadoop.hbase.io.encoding.DataBlockEncoding;
+import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
 import java.util.List;
@@ -32,9 +31,12 @@ public class HbaseConnection {
     }
 
     public void createTable(String tableName, List<String> cols) throws IOException {
-        HBaseAdmin admin = new HBaseAdmin(conf);
-        if(admin.tableExists(tableName))
+        //HBaseAdmin admin = new HBaseAdmin(conf);
+        Admin admin = hConn.getAdmin();
+        if(admin.tableExists(TableName.valueOf(tableName))) {
+            admin.close();
             throw new IOException("table exists");
+        }
         else{
             HTableDescriptor tableDesc = new HTableDescriptor(tableName);
             for(String col : cols) {
@@ -47,13 +49,35 @@ public class HbaseConnection {
         }
     }
 
-    public void saveData(String tableName,List<Put> puts){
-
+    public void saveData(String tableName,List<Put> puts) throws IOException {
+        Table table = hConn.getTable(TableName.valueOf(tableName));
+        table.put(puts);
+        table.close();
     }
 
-    public Result getData(String tableName, String rowket){
+    public Result getData(String tableName, String rowkey) throws IOException {
+        Table table = hConn.getTable(TableName.valueOf(tableName));
+        try {
+            Get get = new Get(Bytes.toBytes(rowkey));
+            Result result = table.get(get);
+            return result;
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            table.close();
+        }
         return null;
     }
 
-
+    public void formatResult(Result result){
+        String rowKey = Bytes.toString(result.getRow());
+        System.out.println("rowKey -> "+rowKey);
+        Cell[] cells = result.rawCells();
+        for(Cell cell : cells){
+            String family = Bytes.toString(CellUtil.cloneFamily(cell));
+            String qualifier = Bytes.toString(CellUtil.cloneQualifier(cell));
+            String value = Bytes.toString(CellUtil.cloneValue(cell));
+            System.out.println("columnfamily -> "+family+":"+qualifier+"  value -> "+value);
+        }
+    }
 }
--- src/main/java/TestHbaseApi.java
+++ src/main/java/TestHbaseApi.java
@@ -1,9 +1,13 @@
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
+
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Created by Administrator on 2017/6/12 0012.
+ * Created by fanzhongyu on 2017/6/12 0012.
  */
 public class TestHbaseApi {
     public static void main (String[] args) throws IOException {
@@ -11,10 +15,22 @@ public class TestHbaseApi {
         String zkServer = "zk-1,zk-2,zk-3";
         String zkPort = "30000";
         HbaseConnection conn = new HbaseConnection(hbaseDir,zkServer,zkPort);
+
         List<String> cols = new LinkedList<String>();
         cols.add("baseinfo");
         cols.add("moreinfo");
         conn.createTable("student",cols);
-
+        List<Put> puts = new LinkedList<Put>();
+        Put put1 = new Put(Bytes.toBytes("Tom"));
+        put1.addColumn(Bytes.toBytes("baseinfo"),Bytes.toBytes("age"),Bytes.toBytes("27"));
+        put1.addColumn(Bytes.toBytes("moreinfo"),Bytes.toBytes("tel"),Bytes.toBytes("111"));
+        Put put2 = new Put(Bytes.toBytes("Jim"));
+        put2.addColumn(Bytes.toBytes("baseinfo"),Bytes.toBytes("age"),Bytes.toBytes("27"));
+        put2.addColumn(Bytes.toBytes("moreinfo"),Bytes.toBytes("tel"),Bytes.toBytes("111"));
+        puts.add(put1);
+        puts.add(put2);
+        conn.saveData("student",puts);
+        Result result = conn.getData("student","Tom");
+        conn.formatResult(result);
     }
 }
